a4436d58ee57bb59e66c3b5965663d7077258fc9
d2sj <kugua_1@163.com>
2024-08-03 18:39:08 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,76 @@
+import { defineConfig } from "vitepress";
+import { set_sidebar } from "./utils/auto_sidebar";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  head: [["link", { rel: "icon", href: "/logo.svg" }]],
+  title: "我的文档项目",
+  description: "A VitePress Site",
+  themeConfig: {
+    //TODO outlineTitle已过时,查看如何使用outline替代 
+    outlineTitle:"目录",
+    outline:[2,6],
+    logo:'/logo.svg',
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      //link一般传绝对路径
+      {
+        text: "家",
+        items: [
+          { text: "首页", link: "/" },
+          { text: "markdown示例", link: "/markdown-examples" },
+        ],
+      },
+      { text: "示例", link: "/markdown-examples" },
+      { text: "自动生成侧边栏", link: "/front-end/react/" },
+      { text: "自动生成侧边栏2", link: "/backend/rebbitmq/" },
+    ],
+
+    // sidebar: [
+    //   {
+    //     text: "Examples",
+    //     items: [
+    //       { text: "Markdown 演示", link: "/markdown-examples" },
+    //       { text: "Runtime API 演示", link: "/api-examples" },
+    //     ],
+    //   },
+    //   {
+    //     text: "Examples",
+    //     items: [
+    //       { text: "Markdown 演示", link: "/markdown-examples" },
+    //       { text: "Runtime API 演示", link: "/api-examples" },
+    //     ],
+    //   },
+    // ],
+    sidebar: {
+       "/front-end/react": set_sidebar("/front-end/react") ,
+       "/backend/rebbitmq": set_sidebar("backend/rebbitmq") ,
+    },
+
+    socialLinks: [{ icon: "github", link: "https://github.com/d2sj" }],
+    footer: {
+      copyright: "Copyright © 2024-present d2sj",
+    },
+
+       // 设置搜索框的样式
+       search: {
+        provider: "local",
+        options: {
+          translations: {
+            button: {
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
+            },
+            modal: {
+              noResultsText: "无法找到相关结果",
+              resetButtonTitle: "清除查询条件",
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+              },
+            },
+          },
+        },
+      },
+  },
+});
---
+++ index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "我的文档项目"
+  text: "一个文档站点"
+  tagline: 三十年河东,三十年河西,再来就六十啦
+  image:
+    src: /404.svg
+    alt: 背景图片
+  actions:
+    - theme: brand
+      text: Markdown 示例
+      link: /markdown-examples
+    - theme: alt
+      text: API 示例
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: 特征1的详细描述
+  - title: Feature B
+    details: 特征2的详细描述
+  - title: Feature C
+    details: 特征3的详细描述
+---
+
